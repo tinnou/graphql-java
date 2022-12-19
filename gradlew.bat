5a828260b3dde1dbb8914bb6264a4c42811acb68
Batch Changes <sourcegraph-batch-changes@netflix.com>
2022-12-19 19:51:10 +0000

    Merge pull request #7 in STE/graphql-java-fork from ~BATCH_CHANGES_USER/ste-graphql-java-fork:source-change/nebula-8-migration to 19.x
    Squashed commit of the following:
    
    commit fccd01cba34f2ada161bfe5a9b28a59aa9eca287
    Author: Batch Changes <sourcegraph-batch-changes@netflix.com>
    Date:   Wed Dec 14 04:18:06 2022 +0000
    
        Migrate to Nebula 8.10.0

--- gradlew.bat
+++ gradlew.bat
@@ -21,7 +21,7 @@ if "%GRADLE_USER_HOME%"=="" (
 )
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+set DEFAULT_JVM_OPTS=
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
@@ -32,6 +32,20 @@ if "%ERRORLEVEL%" == "0" goto init
 
 echo.
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
+goto fail
+
+:findJavaFromJavaHome
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+
+if exist "%JAVA_EXE%" goto init
+
+echo.
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
 echo.
 echo Please set the JAVA_HOME variable in your environment to match the
 echo location of your Java installation.
@@ -52,20 +66,6 @@ set _SKIP=2
 if "x%~1" == "x" goto execute
 
 set CMD_LINE_ARGS=%*
-
-:findJavaFromJavaHome
-set JAVA_HOME=%JAVA_HOME:"=%
-set JAVA_EXE=%JAVA_HOME%/bin/java.exe
-
-if exist "%JAVA_EXE%" goto execute
-
-echo.
-echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
-echo.
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
-
-goto fail
 
 :execute
 @rem Setup the command line
