882702b1ac2a74e5a7b67379b42193556a4e0d55
Antoine Boyer <antoineb@netflix.com>
2024-06-05 15:42:08 -0700

    AstPrinter: Empty types should not include braces `{}` Cherry pick of https://github.com/graphql-java/graphql-java/pull/3619/commits/a9f53dd2c27927093e1c792412008a7ed5039df6 in the meantime of being merged

--- src/main/java/graphql/language/AstPrinter.java
+++ src/main/java/graphql/language/AstPrinter.java
@@ -633,7 +633,7 @@ public class AstPrinter {
 
     private <T extends Node> String block(List<T> nodes) {
         if (isEmpty(nodes)) {
-            return "{}";
+            return "";
         }
         if (compactMode) {
             String joinedNodes = joinTight(nodes, " ", "", "");
